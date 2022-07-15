8270149a268a14180151d5ed03e463fb4e80def5
jhkali87 <jhfakrach87@gmail.com>
2022-07-15 01:17:54 +0100

    front end completed

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/{page}', [App\Http\Controllers\AdminController::class,'index']);
+
+
+
